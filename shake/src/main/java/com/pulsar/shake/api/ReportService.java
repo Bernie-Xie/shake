f6d1309b13975758d41c38d664a3e78abd1abd4c
Bernie-Xie <396323455@qq.com>
2018-05-31 22:37:15 +0800

    test git

---
+++ shake/src/main/java/com/pulsar/shake/api/ReportService.java
@@ -0,0 +1,7 @@
+package com.pulsar.shake.api;
+
+public interface ReportService {
+	
+	String fetchReport(String reportCode);
+	
+}
---
+++ shake/src/main/java/com/pulsar/shake/api/ReportServiceImpl.java
@@ -0,0 +1,9 @@
+package com.pulsar.shake.api;
+
+public class ReportServiceImpl {
+	
+	String fetchReport(String reportCode) {
+		return "";
+	}
+
+}
